c37c8a4888fe86a9d04e7a8d8ff8f9e1f89a5e40
mdrakibul369666 <143260241+mdrakibul369666@users.noreply.github.com>
2023-10-02 15:53:21 +0000

    jhj

--- hi.py
+++ hi.py
@@ -1,2 +1,11 @@
+import sys
 
-print("csv file created for leetcode")
+# Access command-line arguments passed from Express.js
+variable1 = sys.argv[1]
+variable2 = sys.argv[2]
+
+# Perform any operations with the variables
+result = variable1 + ' ' + variable2
+
+# Print the result to be received by Express.js
+print(result)
--- index.js
+++ index.js
@@ -1,15 +1,64 @@
-const express=require('express');
-const app=express();
-const {PythonShell} =require('python-shell');
-app.get("/",(req,res,next)=>{
-    let options = {
-        mode: 'text',
-        args: ['shubhamk314']
-    };
-    PythonShell.run('hi.py', null, function (err, result){
-         console.log(result)
+// const express = require('express');
+// const { PythonShell } = require('python-shell');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   let pyshell = new PythonShell('hi.py');
+
+//   pyshell.on('message', function (message) {
+//     // received a message sent from the Python script (a simple "print" statement)
+//     res.send(message)
+//     pyshell.end(function (err) {
+//       if (err) throw err;
+//       console.log('Python script finished');
+//     });
+//   });
+
+//   pyshell.on('error', function (err) {
+//     console.error(err);
+//     res.status(500).send('An error occurred while executing the Python script');
+//   });
+
+  
+// });
+
+// const port = process.env.PORT || 8000;
+// app.listen(port, () => console.log(`Server connected to ${port}`));
+
+
+const express = require('express');
+const { PythonShell } = require('python-shell');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  let variable1 = 'Hello';
+  let variable2 = 'Boss';
+
+  let pyshell = new PythonShell('hi.py', {
+    args: [variable1, variable2], // Pass variables as arguments to the Python script
+  });
+
+  pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    res.send(message);
+    pyshell.end(function (err) {
+      if (err) throw err;
+      console.log('Python script finished');
     });
-    res.send('hu')
+  });
+
+  pyshell.on('error', function (err) {
+    console.error(err);
+    res.status(500).send('An error occurred while executing the Python script');
+  });
 });
-const port=process.env.PORT||8000;
-app.listen(port,()=>console.log(`Server connected to ${port}`));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server connected to ${port}`));
+
+
+
+
+
